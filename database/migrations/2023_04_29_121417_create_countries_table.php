19a2ea61f521706ba52f909b650a925d2596c4a7
Trin4317 <eg.artourbabaev@gmail.com>
2023-04-29 15:30:48 +0000

    update Country migration file and seeder

--- database/migrations/2023_04_29_121417_create_countries_table.php
+++ database/migrations/2023_04_29_121417_create_countries_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('countries', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->decimal('lat', 10, 7)->nullable();
+            $table->decimal('long', 10, 7)->nullable();
             $table->timestamps();
         });
     }
