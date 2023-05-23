f4dac9f87e87a70e809674d7678d8ee9489e1f32
Trin4317 <eg.artourbabaev@gmail.com>
2023-05-23 09:06:49 +0000

    create RoomType seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,5 +21,6 @@ class DatabaseSeeder extends Seeder
         $this->call(GeoobjectSeeder::class);
 
         $this->call(ApartmentTypeSeeder::class);
+        $this->call(RoomTypeSeeder::class);
     }
 }
